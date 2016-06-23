1681bf7511df7054f18813f60711cf327d64bea5
Facundo Goñi <facundo.goni@hotmail.com>
2016-06-23 14:12:14 -0300

    Tawk.to

--- resources/views/includes/admin/scripts.blade.php
+++ resources/views/includes/admin/scripts.blade.php
@@ -62,3 +62,17 @@
     editor_config.remove_script_host = false;
     editor_config.convert_urls = true;
 </script>
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/576c17d03c4eb124077f2dbf/default';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
