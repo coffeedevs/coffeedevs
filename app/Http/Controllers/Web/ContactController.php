9c27c5c9423bea92980849cd5520156798b68935
Facundo Goñi <facundo.goni@hotmail.com>
2016-08-03 14:55:52 -0300

    Ristretto.

--- app/Http/Controllers/Web/ContactController.php
+++ app/Http/Controllers/Web/ContactController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests;
 use App\Http\Requests\ContactRequest;
 use Illuminate\Support\Facades\Mail;
 
@@ -12,10 +11,10 @@ class ContactController extends Controller
     public function contact(ContactRequest $request)
     {
         $sent = Mail::send('emails.contact', $request->toArray(), function ($mail) use ($request) {
-            $mail->from('contacto@coffeedevs.com', 'Contacto Coffee');
-            $mail->to('coffeedevs@gmail.com')->subject('Tenemos una nueva consulta');
+            $mail->from(env('MAIL_FROM', 'contacto@coffeedevs.com'), 'Contacto Coffee');
+            $mail->to(env('MAIL_TO', 'coffeedevs@gmail.com'))->subject('Tenemos una nueva consulta');
         });
-        if ($sent->getStatusCode() == 200) {
+        if ($sent > 0) {
             $response = [
                 'message' => 'success',
                 'sendstatus' => 1,
@@ -26,6 +25,7 @@ class ContactController extends Controller
                 'sendstatus' => 0,
             ];
         }
+
         return $response;
     }
 }
