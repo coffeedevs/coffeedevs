fad2a7fcc739d48c8779f08b21309875b17f1d61
Facundo Goñi <facundo.goni@hotmail.com>
2016-12-12 15:53:34 -0300

    Agrego image optimizer command.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\OptimizeImages;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +14,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\Generate::class
+        Commands\Generate::class,
+        OptimizeImages::class,
     ];
 
     /**
