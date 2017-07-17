99b7485579458e341e6e1a73f16bbdffba991780
Facundo Goñi <facundo.goni@hotmail.com>
2017-07-17 20:13:32 -0300

    Servicios en español

--- resources/lang/en/web.php
+++ resources/lang/en/web.php
@@ -5,7 +5,91 @@ return [
         'cta' => 'Let\'s work together',
     ],
     'services' => [
-        'title' => 'Services'
+        'title' => 'Services',
+        '1' => [
+            'sub' => 'A QUICK OVERVIEW',
+            'text' => '<p>We provide web development services for quality-conscious clients that understand the value of hiring
+                    professional web developers. We have the technical expertise to architect and deploy your
+                    application as you envision it.</p>
+
+                <p>As entrepreneurs and creative thinkers ourselves, we can help you with much more than web
+                    development. We love helping our clients ideate on optimal features to improve the user experience.
+                    When you work with us, you get our unfiltered feedback and suggestions. We want to see you
+                    succeed!</p>
+
+                <p>If you\'re looking for top notch web development, weekly progress updates on your deliverables, and a
+                    great final product, look no further.</p>'
+        ],
+        '2' => [
+            'sub' => 'OTHER WEB SERVICES',
+            'text' => '<p>We could write a novel about the work we perform. To give you a better idea, here are some additional
+                    services we offer. If you want more information on anything you see here, don\'t hesitate to ask.</p>
+                <ul>
+                    <li>Code reviews</li>
+                    <li>Application debugging and bug fixing</li>
+                    <li>Code maintenance and refactoring</li>
+                    <li>Website security analysis and patching</li>
+                    <li>Website stress testing and performance analysis</li>
+                    <li>Website scaling strategy and implementation</li>
+                    <li>Server setup, updates, maintenance, and deployment</li>
+                </ul>',
+        ],
+        '3' => [
+            'sub' => 'FULL STACK DEVELOPMENT',
+            'text' => '<p>We specialize in building custom web applications from start to finish. This means we handle both
+                    frontend and backend development. If you\'re looking for experts in PHP frameworks such as Laravel
+                    and Symfony or Javascript frameworks like Vue, Angular or jQuery, we should talk.
+                    We encourage the use of modern tools like Gulp and Webpack to automate assets compilation.</p>
+
+                <h2>CUSTOM THIRD PARTY API INTEGRATIONS</h2>
+                <p>Most startups are built on top of at least one API. Make sure you get it done right the first
+                    time.</p>
+
+                <p>We\'ve integrated with countless third party APIs and can tackle yours too! Because we specialize in
+                    building startups, we\'ve experience working with service providers APIs and documentation and
+                    enabling features like social login, automatic billing, geolocalization and more.</p>
+
+                <p>Some of the more common social login APIs we\'ve integrated with are Twitter, Facebook, Github,
+                    Instagram, and Google.</p>',
+        ],
+        '4' => [
+            'sub' => 'WEBSITE OPTIMIZATION AND PERFORMANCE TUNING',
+            'text' => '<p>We have a decade of experience diagnosing and fixing performance issues in websites. We can find
+                    bottlenecks in your web application, database, and web server which are costing you visitors and
+                    money. There are so many possible areas for improvement, we\'re positive we can improve your
+                    performance.
+                </p>
+                <p>
+                    We are confident we can deliver you results, improving the score on common sites like GTMetrix or
+                    Google\'s PageSpeeed Insights, reducing load times of critical pages, and provide you with a
+                    strategic scaling plan for the future. Here\'s just a few things we look for:</p>
+
+                <ul>
+                    <li>Unoptimized or over-sized images on your website.</li>
+                    <li>A poorly configured web server or outdated version of a programming language.</li>
+                    <li>An underpowered or over utilized Linux machine.</li>
+                    <li>Poorly written code resulting in high algorithm complexity.</li>
+                    <li>Poorly written database queries or un-indexed database columns.</li>
+                    <li>Uncompressed CSS and JavaScript files.</li>
+                </ul>',
+        ],
+        '5' => [
+            'sub' => 'SEARCH ENGINE OPTIMIZATION (SEO)',
+            'text' => '<p>We\'d love to analyze and monitor your website to improve it\'s search engine rank. We employ a variety
+                    of tactics including keyword research, SERP keyword rank monitoring, backlink monitoring, monitoring
+                    competitors, and more. We utilize this treasure trove of data to perform on page optimizations to
+                    your site as well as create highly focused landing pages and blog posts to capitalize on increasing
+                    inbound lead generation.
+                </p>',
+        ],
+        '6' => [
+            'sub' => 'MANAGED WEBSITE HOSTING',
+            'text' => '<p>We\'ll gladly work with you to define a hosting solution that fits your applications needs and traffic
+                    patterns. We\'re Linux experts, and can manually configure any number of VPS or dedicated machines
+                    for your startup or company. We take care of hosting and server management, deploying your app on
+                    our DigitalOcean servers. HTTPS? We have you covered. We\'re exceptionally good at performance tuning
+                    a web server to keep your site secure and running fast.</p>',
+        ]
     ],
     'features' => [
         'title' => 'What we do',
@@ -54,5 +138,5 @@ return [
     'portfolio' => [
         'title' => 'Our work',
         'all' => 'All',
-    ]
+    ],
 ];
--- resources/lang/es/web.php
+++ resources/lang/es/web.php
@@ -5,33 +5,111 @@ return [
         'cta' => 'Trabajemos juntos',
     ],
     'services' => [
-        'title' => 'Servicios'
+        'title' => 'Servicios',
+        '1' => [
+            'sub' => 'Una mirada general',
+            'text' => '<p>Proveemos servicios de desarrollo web para clientes concientes de la calidad que entienden el valor
+ de contratar desarrolladores profesionales. Tenemos la pericia técnica para arquitecturar y desplegar tu aplicación como la imaginas.</p>
+
+                <p>Como emprendedores y pensadores creativos nosotros mismos,  podemos ayudarte con mucho más que desarrollo web.
+                Nos encanta ayudar a nuestros clientes a idear características que mejoren la experiencia de usuario.
+                Cuando trabajas con nosotros, recibes nuestras sugerencias y recomendaciones sin filtros. Queremos verte triunfar!
+               </p>
+
+                <p>Si estas buscando desarrolladores web de primera categoría, actualizaciones semanales de tus entregables, y un gran producto final, no busques más.</p>'
+        ],
+        '2' => [
+            'sub' => 'OTROS SERVICIOS WEB',
+            'text' => '<p>Podríamos escribir una novela sobre el trabajo que realizamos
+Para darte una mejor idea, aquí hay solo algunos de los servicios que ofrecemos.
+Si necesitas más información en cualquiera de los items que ves aquí, no dudes en preguntar.</p>
+                <ul>
+                    <li>Revisiones de código</li>
+                    <li>Depuración de aplicaciones y corrección de errores</li>
+                    <li>Mantenimiento y refactorización de código</li>
+                    <li>Análisis y parches de seguridad para tu sitio web</li>
+                    <li>Análisis de rendimiento y pruebas de estrés web</li>
+                    <li>Planificación e implementación de escalado para sitios de alto tráfico</li>
+                    <li>Despliegue, mantenimiento, actualización y configuración de servidores</li>
+                </ul>',
+        ],
+        '3' => [
+            'sub' => 'DESARROLLO "FULL STACK"',
+            'text' => '<p>Nos especializamos en construir aplicaciones web de principio a fin. Esto significa que manejamos tanto desarrollo front-end como back-end.
+Si estás buscando expertos en frameworks PHP como Laravel o Symfony, o frameworks JavaScript como Vue, Angular o jQuery, deberíamos hablar.
+Alentamos el uso de herramientas modernas que automatizan el trabajo con recursos de frontend como Gulp o Webpack.</p>
+
+                <h2>INTEGRACIONES PERSONALIZADAS CON API\'S DE TERCEROS</h2>
+                <p>La mayoría de las startups están construidas encima de por lo menos un API. Asegurate que esté hecho bien a la primera.</p>
+
+                <p>Hemos integrado cantidad incontable de APIs de terceros y podremos con la tuya! Como nos especializamos en construir startups,
+                tenemos experiencia trabajando con APIs de proveedores de servicios, su documentación y en la habilitación de características como 
+                login con redes sociales, facturación automática, geolocalización y mucho más.</p>
+
+                <p>Algunos de los login sociales más comunes que hemos integrado son Twitter, Facebook, Github,
+                    Instagram, y Google.</p>',
+        ],
+        '4' => [
+            'sub' => 'OPTIMIZACIÓN Y PUESTA A PUNTO DE WEBSITES',
+            'text' => '<p>Tenemos décadas de experiencia diagnosticando y corrigiendo problemas de rendimiento en sitios web.
+                        Podemos encontrar cuellos de botella en tus aplicaciones web, base de datos, y servidor que están constandote visitantes y dinero.
+                         Hay tantas posibilidades para mejorar, que somos positivos que podemos mejorar tu rendimiento.
+                </p>
+                <p>Somos confiados en que podemos entregarte resultados, mejorando la puntuación en sitios comúnes como GTMetrix o Google\'s PageSpeed Insights,
+                reduciendo los tiempos de carga de páginas críticas, y proporcionándote con un plan estratégico de escalado para el futuro.
+                Estas son solo algunas de las cosas que vamos a estar mirando:</p>
+
+                <ul>
+                    <li>Imágenes muy grandes o mal optimizadas.</li>
+                    <li>Servidor web mal configurado o lenguaje de programación utilizando una versión anticuada.</li>
+                    <li>Servidor Linux sobreutilizado o muy chico.</li>
+                    <li>Código pobremente escrito, resultando en algoritmos de alta complejidad y bajo rendimiento.</li>
+                    <li>Consultas a la base de datos poco optimizadas, o columnas consultadas sin indizar.</li>
+                    <li>Archivos JS y CSS sin comprimir y minificar.</li>
+                </ul>',
+        ],
+        '5' => [
+            'sub' => 'OPTIMIZACIÓN PARA MOTORES DE BÚSQUEDA (SEO)',
+            'text' => '<p>Nos encantaría analizar y monitorear tu sitio para mejorar su ránking de búsqueda.
+            Empleamos una variedad de tácticas que incluyen investigación de palabras clave, monitoreo del ranking de palabras clave en SERP (páginas de resultado),
+            monitoreo de enlaces de retroceso (backlinks), monitoreo de competidores y más. Utilizamos el descubrimiento de esta preciada información para realizar
+            optimizaciones en las páginas de tu sitio, así como creamos landing pages y publicaciones de blog altamente enfocadas para capitalizar en el incremento
+            de links generados desde adentro (Inbound marketing).
+                </p>',
+        ],
+        '6' => [
+            'sub' => 'HOSTING WEB ADMINISTRADO',
+            'text' => '<p>Con gusto trabajaremos contigo para definir una solución de alojamiento que encaja con las necesidades de tus aplicaciones y sus patrones de tráfico.
+Somos expertos en Linux, y podemos configurar manualmente cualquier número de VPS o máquinas dedicadas que tenga su compañia o startup.
+Nos encargamos del alojamiento y la administración de los servidores, desplegando su aplicación en nuestros servidores en DigitalOcean o en-sitio.
+HTTPS? Lo tenemos cubierto. Somos excepcionales en ajustar el rendimiento de un servidor web para mantener su sitio seguro e increíblemente rápido.</p>',
+        ]
     ],
     'features' => [
         'title' => 'Lo que hacemos',
         '0' => [
             'title' => 'CONSTRUIMOS',
-            'text' => 'We build full fledged MVPs for entrepreneurs and startups looking to turn their ideas into reality.'
+            'text' => 'Construimos MVPs (Producto mínimo viable) totalmente desarrollados para emprendedores y startups buscando transformar sus ideas en realidad.'
         ],
         '1' => [
             'title' => 'AUTOMATIZAMOS',
-            'text' => 'Learn how to install, manage, backup and configure the most important part of your applications.'
+            'text' => 'Aprende como instalar, manejar, resguardar y configurar los aspectos más importantes de tus aplicaciones.'
         ],
         '2' => [
             'title' => 'OPTIMIZAMOS',
-            'text' => 'We increase your online sales by updating copy, optimizing calls to action, and creating measurable traffic funnels.'
+            'text' => 'Incrementamos tus ventas online mediante una actualización de tu texto de marketing, optimizando llamadas a la acción, y creando canales de tráfico medibles.'
         ],
         '3' => [
             'title' => 'SEGUIMOS',
-            'text' => 'We create KPIs tied to your goals and teaching you to track your progress and achievements with metrics.'
+            'text' => 'Creamos KPIs atados a tus objetivos y te enseñamos como seguir tu progreso y tus logros con métricas.'
         ],
         '4' => [
             'title' => 'ARREGLAMOS',
-            'text' => 'We fix bugs and poorly written code, especially the kind that stemmed from your previous, outsourced development team.'
+            'text' => 'Corregimos errores y código mal escrito, especialmente el derivado de tu anterior equipo de desarollo.'
         ],
         '5' => [
             'title' => 'MEJORAMOS',
-            'text' => 'We work on or maintain existing and legacy projects for creative and marketing agencies that need extra manpower or expertise.'
+            'text' => 'Trabajamos en o mantenemos proyectos existentes y legacy para agencias de marketing y creativos que necesitan un apoyo extra de recursos humanos o expertise.'
         ],
     ],
     'blog' => [
