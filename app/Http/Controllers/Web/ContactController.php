7da9d36efb53d056f7b1a4109155cd503fafd7fa
Martin Guadalupe <martinher87@hotmail.com>
2017-10-01 13:13:16 -0300

    Arreglo en envio de mail

--- app/Http/Controllers/Web/ContactController.php
+++ app/Http/Controllers/Web/ContactController.php
@@ -5,21 +5,23 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ContactRequest;
 use Illuminate\Support\Facades\Mail;
+use Mockery\Exception;
 
 class ContactController extends Controller
 {
     public function contact(ContactRequest $request)
     {
-        $sent = Mail::send('emails.contact', $request->toArray(), function ($mail) use ($request) {
-            $mail->from(env('MAIL_FROM', 'contacto@coffeedevs.com'), 'Contacto Coffee');
-            $mail->to(env('MAIL_TO', 'coffeedevs@gmail.com'))->subject('Tenemos una nueva consulta');
-        });
-        if ($sent > 0) {
+        try {
+            $sent = Mail::send('emails.contact', $request->toArray(), function ($mail) use ($request) {
+                $mail->from(env('MAIL_FROM', 'contacto@coffeedevs.com'), 'Contacto Coffee');
+                $mail->to(env('MAIL_TO', 'coffeedevs@gmail.com'))->subject('Tenemos una nueva consulta');
+            });
+
             $response = [
                 'message' => 'success',
                 'sendstatus' => 1,
             ];
-        } else {
+        } catch (Exception $ex) {
             $response = [
                 'message' => 'error',
                 'sendstatus' => 0,
